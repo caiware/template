b16e2e82c877ac8a299ad570cb3ae6e8ef3f40ea
caiware <caiweipeng@terminal.sz>
2021-02-04 14:08:05 +0800

    “1.0”

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { Button } from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+Vue.component(Button.name, Button)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
